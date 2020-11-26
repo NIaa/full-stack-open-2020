414dbc1405e5c1b8ba096ecab12d9c86ebc5eb69
NIaa <12479981+NIaa@users.noreply.github.com>
2020-11-26 14:39:51 +0800

    courseinfo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => {
-    return <h1>{props.course.title}</h1>
+    return <h1>{props.course.name}</h1>
 }
 
 const Part = (props) => {
@@ -29,7 +29,7 @@ const Total = (props) => {
 
 const App = () => {
     const course = {
-        title: 'Half Stack application development',
+        name: 'Half Stack application development',
         parts: [
             {name: 'Fundamentals of React',    exercises: 10},
             {name: 'Using props to pass data', exercises:  7},
