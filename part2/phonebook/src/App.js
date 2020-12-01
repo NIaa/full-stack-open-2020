1a33baf8fb5f0020aa9203a74f62502529cea534
NIaa <12479981+NIaa@users.noreply.github.com>
2020-12-01 19:05:54 +0800

    phonebook all complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,44 +1,87 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import './index.css'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
+import pbServices from './services/phonebook'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
-
+  const [ newNotification, setNotification ] = useState(null)
+  const [ newClassName, setClassName ] = useState('notification')
   
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.map(p => p.name).indexOf(newName) !== -1) {
-      window.alert(`${newName} is already added to phonebook`)
-      return
+    const index = persons.map(p => p.name).indexOf(newName)
+    if (index !== -1) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
+        pbServices.update(persons[index].id, {...persons[index], number: newNumber})
+          .then(updatedPerson => {
+            setPersons(persons.map(p => p.id !== updatedPerson.id ? p : updatedPerson))
+          })
+          .catch(error => {
+            setClassName('error')
+            setNotification(`Information of ${persons[index].name} has already been removed from server`)
+            setPersons(persons.filter(p => p.id !== persons[index].id))            
+            setTimeout(() => {
+              setNotification(null)
+            }, 5000) 
+          })
+          return
+      }
     }
-    const newObject = { name:newName, number:newNumber }
-    setPersons(persons.concat(newObject))
-    setNewName('')
+    const newObject = { 
+      name: newName, 
+      number: newNumber
+    }
+    pbServices
+      .create(newObject)
+      .then(returnedPerson => {
+        console.log(returnedPerson)
+        setPersons(persons.concat(returnedPerson))
+        setClassName('notification')
+        setNotification(`Added ${newName}`)
+        setNewName('')
+        setNewNumber('')
+         setTimeout(() => {
+          setNotification(null)
+        }, 5000)        
+      })
+      .catch(error => {console.log(error)})
   }
 
   const handleNameChange = (event) => setNewName(event.target.value)
-
   const handleNumberChange = (event) => setNewNumber(event.target.value)
-  
   const handleFilterChange = (event) => setNewFilter(event.target.value)
   
+  useEffect(() => {
+    pbServices
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+
+  const handleClick = (name, id) => {
+    /* good example for a promise chain */
+    if (window.confirm(`Delete ${name}?`)) {
+      pbServices
+        .deletePerson(id) /* a promise */
+        .then(response => pbServices.getAll())
+        .then(deletedPersons => setPersons(deletedPersons))
+        .catch(error => console.log(error))
+  }}
 
   return (
     <div>
       <h2>Phonebook</h2>
+        <Notification className={newClassName} message={newNotification}/>
         <Filter newFilter={newFilter} handleFilterChange={handleFilterChange}/>
       <h2>add a new</h2>
-
       <PersonForm 
         addPerson={addPerson} 
         newName={newName} 
@@ -47,9 +90,14 @@ const App = () => {
         handleNumberChange={handleNumberChange} 
       />
       <h2>Numbers</h2>
-      <Persons persons={persons} newFilter={newFilter}/>
+      <Persons 
+        persons={persons} 
+        newFilter={newFilter}
+        handleClick={handleClick}
+      />
     </div>
   )
 }
+
 
 export default App
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,11 +1,21 @@
 import React from "react"
-const Person = ({name, number}) => <p>{name} {number}</p>
 
-const Persons = ({persons, newFilter}) => { 
-  const filtered = persons.filter(p => p.name.toLowerCase().includes(newFilter.toLowerCase()))
+const Person = ({name, number, id, handleClick}) => 
+  <p>{name} {number} <button onClick={() => handleClick(name, id)}>delete</button></p>
+
+const Persons = ({persons, newFilter, handleClick}) => { 
+  const filtered = persons
+    .filter(p => p.name.toLowerCase().includes(newFilter.toLowerCase()))
   return (
   <div>
-    {filtered.map(p => <Person key={p.name} name={p.name} number={p.number}/>)}
+    {filtered.map(p => 
+      <Person 
+        key={p.id} 
+        name={p.name} 
+        number={p.number} 
+        id={p.id} 
+        handleClick={handleClick}
+      />)}
   </div>
 )}
 
